49a6edd0284c649116a97e6d8c881d660a164279
Steven Kristianto <stevenkristianto97@yahoo.com>
2020-03-16 14:40:44 +0700

    start develop

---
+++ src/main/java/com/bos/virtualaccount/VirtualaccountApplication.java
@@ -0,0 +1,13 @@
+package com.bos.virtualaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualaccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualaccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bos/virtualaccount/VirtualaccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bos.virtualaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualaccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
